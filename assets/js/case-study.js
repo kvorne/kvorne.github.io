3fbe520b226b1d0522466ef9a558061f87744ef9
kvorne <katarinavorne@gmail.com>
2024-06-24 14:54:25 -0700

    Added Vorne Case study

--- assets/js/case-study.js
+++ assets/js/case-study.js
@@ -1,20 +1,3 @@
-// // Secondary navbar
-// window.onscroll = function() {scrollFunction()};
-
-// const secondaryNav = document.getElementById("secondary-navbar");
-
-// var sticky = secondaryNav.getBoundingClientRect().top;
-
-
-// function scrollFunction() {
-//     if (this.oldScroll < this.scrollY && (document.body.scrollTop > sticky || document.documentElement.scrollTop > 20)) {
-//         document.getElementById("secondary-navbar").style.top = "55px";
-//     } else {
-//         // document.getElementById("secondary-navbar").style.top = "81px";
-//     }
-//   this.oldScroll = this.scrollY;
-// }
-
 const borger = document.querySelector(".hamburgerMenu");
 const navBar = document.getElementById("navbar");
 const navList = document.querySelector(".navList");
