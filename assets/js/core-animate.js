be1bce641080f5b7b66e5a368025201172605006
kvorne <katarinavorne@gmail.com>
2024-07-10 17:08:17 -0500

    revert after timeline complete

--- assets/js/core-animate.js
+++ assets/js/core-animate.js
@@ -124,31 +124,26 @@ faders.forEach( fader => {
 /* h1 animation By Adam Robinson from Codepen: https://codepen.io/adamrobinson/pen/BZWwdR */
 function init() {
     gsap.registerPlugin(SplitText) 
-        var $heading = $("h1");
-        var splitTextTimeline = new TimelineLite({paused: true});
+    var $heading = $("h1");
+    var splitTextTimeline = new TimelineLite({paused: true, onComplete: () => {split.revert()}});
 
-        var split = new SplitText($heading, {
-            type: "lines", 
-            linesClass: "line", 
-            position: "relative",
-            onComplete:allDone
-        });
+    var split = new SplitText($heading, {
+        type: "lines", 
+        linesClass: "line", 
+        position: "relative",
+    });
 
-        function allDone() {
-            split.revert()
-        }
+    /*
+    * Create a custom ease https://greensock.com/ease-visualizer
+    */
+    CustomEase.create("customEase", "M0,0 C0.2,0 0.337,0.566 0.47,0.772 0.615,0.998 0.822,1 1,1");
 
-        /*
-        * Create a custom ease https://greensock.com/ease-visualizer
-        */
-        CustomEase.create("customEase", "M0,0 C0.2,0 0.337,0.566 0.47,0.772 0.615,0.998 0.822,1 1,1");
+    $('.line').wrapInner("<div class='line-inner'></div>")
 
-        $('.line').wrapInner("<div class='line-inner'></div>")
+    var $headingInner = $(".line-inner");
 
-        var $headingInner = $(".line-inner");
-
-        splitTextTimeline.staggerTo($headingInner, 0.7, { y:0,  force3D:true, ease: "customEase"}, 0.1);
-        splitTextTimeline.play();
+    splitTextTimeline.staggerTo($headingInner, 0.7, { y:0,  force3D:true, ease: "customEase"}, 0.1);
+    splitTextTimeline.play();
 }
 
 document.addEventListener("DOMContentLoaded", (event) => {
@@ -166,7 +161,7 @@ function debounce(func){
 }
 
 window.addEventListener("resize",debounce(function(e){
-    split.revert()
+    splitTextTimeline.revert()
 }));
 
 init();
